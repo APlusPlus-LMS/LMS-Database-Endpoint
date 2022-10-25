5c2ab40428576408273f619ecce452600c1b7291
multimokia <multimokia3@gmail.com>
2022-10-25 12:38:13 -0400

    cleaning up + status

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,20 @@ import { connect } from "mongoose";
 dotenv.config();
 
 
-export async function connectDB() {
+export async function connectDB(): Promise<void> {
     return new Promise((resolve, reject) => {
         if (process.env.MONGODB_URI === undefined) {
             return reject("MONGODB_URI not set, please provide a valid database url.");
         }
 
-        connect(process.env.MONGODB_URI).then(resolve).catch(reject);
+        connect(process.env.MONGODB_URI)
+            .then(() => {
+                console.log("Connected to database.");
+                resolve();
+            })
+            .catch(err => {
+                console.error(`Failed to connect to database: ${err}`);
+                reject(err);
+            });
     });
 }
